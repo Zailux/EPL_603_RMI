ca2b5e112dfad92289c8d36b6420f122597abfff
Andreas Andreou <adandreoucy@gmail.com>
2021-11-29 16:08:21 +0200

    More patient methods

--- src/rmi/services/patient/PatientServant.java
+++ src/rmi/services/patient/PatientServant.java
@@ -6,6 +6,8 @@ import rmi.models.patient.PatientDaoImplementation;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PatientServant extends UnicastRemoteObject implements PatientService {
 
@@ -20,5 +22,29 @@ public class PatientServant extends UnicastRemoteObject implements PatientServic
         PatientDaoImplementation patDao = new PatientDaoImplementation();
         patDao.add(patient);
         return patient;
+    }
+
+    @Override
+    public Patient updatePatient(Integer id, String name, String address, Boolean historyOfSelfHarm, String riskIndicator) throws RemoteException, SQLException {
+        Patient patient = new Patient(name, address, historyOfSelfHarm, riskIndicator);
+        patient.setId(id);
+
+        PatientDaoImplementation patDao = new PatientDaoImplementation();
+        patDao.update(patient);
+        return patient;
+    }
+
+    @Override
+    public Patient fetchPatient(Integer id, String name) throws RemoteException, SQLException {
+        PatientDaoImplementation patDao = new PatientDaoImplementation();
+        Patient patient = patDao.getPatient(id);
+        return patient;
+    }
+
+    @Override
+    public List<Patient> fetchPatients() throws RemoteException, SQLException {
+        PatientDaoImplementation patDao = new PatientDaoImplementation();
+        List<Patient> patients = patDao.getPatients();
+        return patients;
     }
 }
--- src/rmi/services/patient/PatientService.java
+++ src/rmi/services/patient/PatientService.java
@@ -5,7 +5,14 @@ import rmi.models.patient.Patient;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface PatientService extends Remote {
     public Patient createPatient(String name, String address, Boolean historyOfSelfHarm, String riskIndicator) throws RemoteException, SQLException;
+
+    public Patient updatePatient(Integer id, String name, String address, Boolean historyOfSelfHarm, String riskIndicator) throws RemoteException, SQLException;
+
+    public Patient fetchPatient(Integer id, String name) throws RemoteException, SQLException;
+
+    public List<Patient> fetchPatients() throws RemoteException, SQLException;
 }
