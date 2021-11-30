b08a7bcefd009f676e0312e3cd8994857d0cd76e
Solonas <solonas.georgiou@gmail.com>
2021-11-30 20:17:01 +0200

    commit

--- src/frontend/ClinicalStaff.java
+++ src/frontend/ClinicalStaff.java
@@ -6,6 +6,10 @@ import javax.swing.text.StyleContext;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Locale;
 
 public class ClinicalStaff {
@@ -14,7 +18,11 @@ public class ClinicalStaff {
     private JButton updateRecordsButton;
     public JLabel result;
     private JButton EXITButton;
+    private JButton createTreatmentButton;
+    private JButton createConsultationButton;
+    private JButton searchSideEffectsButton;
     public static JFrame frame1 = new JFrame("Clinical Staff");
+
 
     public ClinicalStaff() {
         String myArray[] = new String[2];
@@ -25,11 +33,12 @@ public class ClinicalStaff {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String value = "";
+                int id = 1;
 
                 //Object id = JOptionPane.showInputDialog(frame1, "Enter patient id:");
-                String id = JOptionPane.showInputDialog(frame1, "Enter patient id:");
-                int idgiven = Integer.parseInt(id);
-                if (idgiven == 1) {
+                String ids = JOptionPane.showInputDialog(frame1, "Enter patient id:");
+                int idgiven = Integer.parseInt(ids);
+                if (idgiven == id) {
                     for (int j = 0; j < myArray.length; j++) {
                         value += myArray[j] + "  ";
                     }
@@ -45,33 +54,37 @@ public class ClinicalStaff {
                 String id = JOptionPane.showInputDialog(frame1, "Enter patient id:");
                 int idgiven = Integer.parseInt(id);
                 JLabel label = new JLabel("");
-                JTextField field1 = new JTextField("");
-                JTextField field2 = new JTextField("");
                 JTextField field3 = new JTextField("");
+
 
                 JPanel panel = new JPanel(new GridLayout(10, 10));
                 panel.add(new JLabel("Update for patient " + id));
 
-                panel.add(new JLabel("Consultation details: "));
-                Component nc = panel.add(field1);
-                String newconsultation = nc.toString();
 
-                panel.add(new JLabel("Treatment prescribed:"));
-                Component nt = panel.add(field2);
-                String newtreatment = nt.toString();
+                panel.add(new JLabel("Create Consultation for patient " + id));
+                String t[] = {
+                        "mental", "physical", "viral", "other"};
+                JComboBox cb = new JComboBox(t);
+                panel.add(cb);
 
                 panel.add(new JLabel("Comment: "));
                 Component com = panel.add(field3);
                 String comment = com.toString();
 
+                Date curruntdate = Date.from(Instant.now());
+                System.out.println(curruntdate);
+
+                boolean finished = true;
 
                 int result = JOptionPane.showConfirmDialog(null, panel, "Update Patient",
                         JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
                 if (result == JOptionPane.OK_OPTION) {
+                    String data = "Selected: " + cb.getItemAt(cb.getSelectedIndex());
+                    System.out.println(data);
+
                     JOptionPane.showMessageDialog(null, "Updated successfully for patient " + id);
-                    System.out.println(" " + field1.getText()
-                            + " " + field2.getText());
+                    System.out.println(field3.getText());
                 } else {
                     JOptionPane.showMessageDialog(null, "Nothing change for patient " + id);
                 }
@@ -81,6 +94,100 @@ public class ClinicalStaff {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
+            }
+        });
+
+        createTreatmentButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id = JOptionPane.showInputDialog(frame1, "Enter patient id:");
+                int idgiven = Integer.parseInt(id);
+                //user id
+
+                JLabel label = new JLabel("");
+                JTextField field3 = new JTextField("");
+
+                JPanel panel = new JPanel(new GridLayout(10, 10));
+
+                panel.add(new JLabel("Description: "));
+                Component com = panel.add(field3);
+                String descr = com.toString();
+
+                Date curruntdate = Date.from(Instant.now());
+
+                int result = JOptionPane.showConfirmDialog(null, panel, "Update Patient",
+                        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+                if (result == JOptionPane.OK_OPTION) {
+                    System.out.println(curruntdate);
+                    JOptionPane.showMessageDialog(null, "Updated successfully for patient " + id);
+                    System.out.println(" " + field3.getText());
+                } else {
+                    JOptionPane.showMessageDialog(null, "Nothing change for patient " + id);
+                }
+
+            }
+        });
+
+        createConsultationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id = JOptionPane.showInputDialog(frame1, "Enter patient id:");
+                int idgiven = Integer.parseInt(id);
+                //user id
+
+                JLabel label = new JLabel("");
+                JTextField field1 = new JTextField("");
+                JTextField field2 = new JTextField("");
+                JTextField field3 = new JTextField("");
+
+                JPanel panel = new JPanel(new GridLayout(10, 10));
+                panel.add(new JLabel("Create Consultation for patient " + id));
+                String t[] = {
+                        "mental", "physical", "viral", "other"};
+                JComboBox cb = new JComboBox(t);
+                panel.add(cb);
+
+                panel.add(new JLabel("Comment: "));
+                Component com = panel.add(field3);
+                String comment = com.toString();
+
+                Date curruntdate = Date.from(Instant.now());
+
+                boolean finished = true;
+
+                int result = JOptionPane.showConfirmDialog(null, panel, "Update Patient",
+                        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+                if (result == JOptionPane.OK_OPTION) {
+                    String data = "Selected: " + cb.getItemAt(cb.getSelectedIndex());
+                    System.out.println(data);
+                    System.out.println(curruntdate);
+                    JOptionPane.showMessageDialog(null, "Updated successfully for patient " + id);
+                    System.out.println(" " + field3.getText());
+                } else {
+                    JOptionPane.showMessageDialog(null, "Nothing change for patient " + id);
+                }
+
+
+            }
+        });
+
+        searchSideEffectsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int id = 1;
+                String value = "";
+                String ids = JOptionPane.showInputDialog(frame1, "Enter medicine id:");
+                int idgiven = Integer.parseInt(ids);
+
+                if (idgiven == id) {
+                    for (int j = 0; j < myArray.length; j++) {
+                        value += myArray[j] + "    ";
+                    }
+                    result.setText(value);
+                }
             }
         });
     }
@@ -95,7 +202,6 @@ public class ClinicalStaff {
     public static void closewin() {
         frame1.dispose();
     }
-
 
     {
 // GUI initializer generated by IntelliJ IDEA GUI Designer
@@ -113,7 +219,7 @@ public class ClinicalStaff {
      */
     private void $$$setupUI$$$() {
         ClinicalStaff = new JPanel();
-        ClinicalStaff.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(3, 3, new Insets(0, 0, 0, 0), -1, -1));
+        ClinicalStaff.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(4, 3, new Insets(0, 0, 0, 0), -1, -1));
         viewPatientRecordsButton = new JButton();
         viewPatientRecordsButton.setText("View patient records");
         ClinicalStaff.add(viewPatientRecordsButton, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
@@ -121,10 +227,19 @@ public class ClinicalStaff {
         Font resultFont = this.$$$getFont$$$(null, -1, 16, result.getFont());
         if (resultFont != null) result.setFont(resultFont);
         result.setText("");
-        ClinicalStaff.add(result, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 3, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(37, 29), null, 0, false));
+        ClinicalStaff.add(result, new com.intellij.uiDesigner.core.GridConstraints(3, 0, 1, 3, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(37, 29), null, 0, false));
+        createTreatmentButton = new JButton();
+        createTreatmentButton.setText("Create treatment");
+        ClinicalStaff.add(createTreatmentButton, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         updateRecordsButton = new JButton();
-        updateRecordsButton.setText("Update records ");
+        updateRecordsButton.setText("Update consultation ");
         ClinicalStaff.add(updateRecordsButton, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        searchSideEffectsButton = new JButton();
+        searchSideEffectsButton.setText("Search side effects");
+        ClinicalStaff.add(searchSideEffectsButton, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        createConsultationButton = new JButton();
+        createConsultationButton.setText("Create consultation");
+        ClinicalStaff.add(createConsultationButton, new com.intellij.uiDesigner.core.GridConstraints(1, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         EXITButton = new JButton();
         Font EXITButtonFont = this.$$$getFont$$$(null, Font.BOLD, 20, EXITButton.getFont());
         if (EXITButtonFont != null) EXITButton.setFont(EXITButtonFont);
@@ -160,5 +275,6 @@ public class ClinicalStaff {
     public JComponent $$$getRootComponent$$$() {
         return ClinicalStaff;
     }
-
 }
+
+
